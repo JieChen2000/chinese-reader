13ecacdd72da6fb0f563f8d077933e121fe4cb0d
Jay Chen <jie.ch.2000@gmail.com>
2020-04-27 22:28:16 -0500

    Add feature to count unknown words

--- read_chinese.py
+++ read_chinese.py
@@ -3,4 +3,13 @@ f = open('input.txt')
 words = f.read().rstrip()
 words = words.replace('\n','')
 words_shuffle = ''.join(random.sample(words, len(words)))
-print(set(words_shuffle))   # set make it unique
+words_set = set(words_shuffle) # set make it unique
+words_set_not_command = set()
+print("We have ", len(words_set), " words learned, let's review them one by one, enter n if you don't know it")
+for val in words_set:
+    print(val)   
+    user_input = input()
+    # print(user_input)
+    if user_input == 'n':
+        words_set_not_command.add(val)
+print("Here're the words you need work more on\n", words_set_not_command)
