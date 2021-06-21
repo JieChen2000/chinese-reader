edbf7ae8ae594576a2907c33aa2b128c2cd30247
Jay Chen <jie.ch.2000@gmail.com>
2021-06-21 14:33:08 -0500

    Enable auto save uncommanded words

--- read_chinese.py
+++ read_chinese.py
@@ -1,5 +1,7 @@
 import random
 import argparse
+import os
+from datetime import datetime 
 if __name__ == '__main__':
     """
     Randomly display a word(from arg words_lib_name file) to check if student knows it. 
@@ -24,18 +26,24 @@ if __name__ == '__main__':
     words_set = set(words_shuffle) # set make it unique
     words_set_not_command = set()
     print("We have ", len(words_set), " words learned, let's review them one by one, enter n if you don't know it")
+    text_file = open("input_not_commanded.txt", "a")
+
     for i, val in enumerate(words_set):
         print('word ', i, ':', val)   
         user_input = input()
         # print(user_input)
         if user_input == 'n':
             words_set_not_command.add(val)
+            text_file.write(val)
+
     print("Great job, you've commanded ",len(words_set)-len(words_set_not_command), " words out of total ", len(words_set), " or ", 100-len(words_set_not_command)*100./len(words_set), "% completed!")        
     print("Here're the words you need work more on\n", words_set_not_command)
-    print("Do you like to save these words to a new words_libaray file? [y/N]:")
-    user_input = input()
-    if user_input == 'y' or user_input == 'Y':
-        text_file = open("input_not_commanded.txt", "w")
-        text_file.write(''.join(words_set_not_command))
-        print("Commanded words saved to ", text_file.name)
-        text_file.close()
+    print("You only need work on these ", len(words_set_not_command), " words next time, Congratulations!")
+    text_file.close()
+    now = datetime.now()
+    dest_file_name = 'data/input_'+now.strftime("%m%d%Y_%H%M%S")+'.txt'
+    if not os.path.isdir('data'):
+        os.system('mkdir data')
+
+    os.rename('input.txt', dest_file_name)
+    os.rename('input_not_commanded.txt', 'input.txt')
